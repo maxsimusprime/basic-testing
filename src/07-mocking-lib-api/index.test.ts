3832d4ef67eeac9860647f7f113ab731546ccf61
maxsimusprime <96006023+maxsimusprime@users.noreply.github.com>
2024-02-12 23:47:36 +0300

    feat: update 07-mocking-lib-api test

--- src/07-mocking-lib-api/index.test.ts
+++ src/07-mocking-lib-api/index.test.ts
@@ -1,17 +1,55 @@
-// Uncomment the code below and write your tests
-/* import axios from 'axios';
-import { throttledGetDataFromApi } from './index'; */
+import axios from 'axios';
+import { throttledGetDataFromApi } from './index';
+
+jest.mock('axios');
 
 describe('throttledGetDataFromApi', () => {
+  beforeAll(() => {
+    jest.useFakeTimers();
+  });
+
+  afterAll(() => {
+    jest.useRealTimers();
+  });
+
   test('should create instance with provided base url', async () => {
-    // Write your test here
+    axios.create = jest.fn(() => axios);
+    axios.get = jest.fn().mockResolvedValue('responseData');
+
+    const spyOnAxiosCreate = jest.spyOn(axios, 'create');
+
+    jest.advanceTimersByTime(5000);
+
+    await throttledGetDataFromApi('/relativePAth');
+
+    expect(spyOnAxiosCreate).toHaveBeenCalledWith({
+      baseURL: 'https://jsonplaceholder.typicode.com',
+    });
   });
 
   test('should perform request to correct provided url', async () => {
-    // Write your test here
+    axios.create = jest.fn(() => axios);
+    axios.get = jest.fn().mockResolvedValue('responseData');
+
+    const spyOnAxiosGet = jest.spyOn(axios, 'get');
+
+    jest.advanceTimersByTime(5000);
+
+    await throttledGetDataFromApi('/relativePAth');
+
+    expect(spyOnAxiosGet).toHaveBeenCalledWith('/relativePAth');
   });
 
   test('should return response data', async () => {
-    // Write your test here
+    axios.create = jest.fn(() => axios);
+    axios.get = jest
+      .fn()
+      .mockResolvedValue({ status: 200, data: 'responseData' });
+
+    jest.advanceTimersByTime(5000);
+
+    const response = await throttledGetDataFromApi('relativePAth');
+
+    expect(response).toBe('responseData');
   });
 });
